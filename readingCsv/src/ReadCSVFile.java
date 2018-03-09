4e50ff51970c7e59bfde27823b4c359707645870
Nyolczas <nyolczas@gmail.com>
2018-03-09 21:03:58 +0100

    ReadCSVfile

---
+++ readingCsv/src/ReadCSVFile.java
@@ -0,0 +1,28 @@
+package com.vn;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class ReadCSVFile {
+
+    public static void main(String[] args){
+        String COMMA_DELIMITER = ",";
+        BufferedReader bufReader = null;
+        try {
+            bufReader = new BufferedReader(new FileReader("C:/Users/User/Documents/Tozsde/MT4ek/XM1/MQL4/Files/XM_19061180.csv"));
+            String rowData = "";
+            //bufReader.readLine();
+            String[] dataByRow;
+            while((rowData = bufReader.readLine()) != null){
+                dataByRow = rowData.split(",");
+
+                for(String s: dataByRow){
+                    System.out.println(s + " - ");
+                }
+            }
+        } catch (Exception e)
+        {
+
+        }
+    }
+}
--- readingCsv/src/csvDemo.java
+++ readingCsv/src/csvDemo.java
@@ -1,7 +1,27 @@
+import com.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.util.Arrays;
+
 public class csvDemo {
-    public class csvDemo{
+
         public static void main(String[] args) {
-            
+
+            try{
+
+                CSVReader reader = new CSVReader(new FileReader("C:/Users/User/Documents/Tozsde/MT4ek/XM1/MQL4/Files/XM_19061180.csv"));
+                String[]nextline;
+                while((nextline = reader.readNext()) != null)
+                {
+                    if(nextline != null)
+                    {
+                        System.out.println(Arrays.toString(nextline));
+                    }
+                }
+            }catch(Exception e)
+            {
+                System.out.println(e);
+            }
+            System.out.println("CSV Read complete");
         }
-    }
 }
