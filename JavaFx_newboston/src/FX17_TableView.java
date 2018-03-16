37c8041c02e0e58ae2799554e7d0eae9214d0168
Nyolczas <nyolczas@gmail.com>
2018-03-16 19:45:28 +0100

    Table view

--- JavaFx_newboston/src/FX17_TableView.java
+++ JavaFx_newboston/src/FX17_TableView.java
@@ -15,6 +15,8 @@ import javafx.stage.Stage;
 public class FX17_TableView extends Application{
 
     Stage window;
+    TableView<FX17b_Stategy> table;
+
 
     public static void main(String[] args){
         launch(args);
@@ -25,11 +27,41 @@ public class FX17_TableView extends Application{
         window = primaryStage;
         window.setTitle("Table View Demo");
 
+        // Name column
+        TableColumn<FX17b_Stategy, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setMinWidth(200);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        // hozam column
+        TableColumn<FX17b_Stategy, Double> hozamColumn = new TableColumn<>("Hozam");
+        hozamColumn.setMinWidth(100);
+        hozamColumn.setCellValueFactory(new PropertyValueFactory<>("hozam"));
+
+        // dbTrade column
+        TableColumn<FX17b_Stategy, Integer> dbTradeColumn = new TableColumn<>("DbTrade");
+        dbTradeColumn.setMinWidth(100);
+        dbTradeColumn.setCellValueFactory(new PropertyValueFactory<>("dbTrade"));
+
+        table = new TableView<>();
+        table.setItems(getFX17b_Strategy());
+        table.getColumns().addAll(nameColumn, hozamColumn, dbTradeColumn);
+
         VBox vBox = new VBox();
-        vBox.getChildren().addAll();
+        vBox.getChildren().addAll(table);
 
         Scene scene = new Scene(vBox);
         window.setScene(scene);
         window.show();
     }
+
+    // Get all of the Strategies -- ebbe kell beolvasni a CSV-t!
+    public ObservableList<FX17b_Stategy> getFX17b_Strategy(){
+        ObservableList<FX17b_Stategy> strategies = FXCollections.observableArrayList();
+        strategies.add(new FX17b_Stategy("M5_ADX", 858.94, 20));
+        strategies.add(new FX17b_Stategy("M15_ADX", 1250.87, 45));
+        strategies.add(new FX17b_Stategy("M15_TSQ", 40.52, 34));
+        strategies.add(new FX17b_Stategy("H_Classic", 250.42, 94));
+        strategies.add(new FX17b_Stategy("H_T200", -10.85, 15));
+        return strategies;
+     }
 }
