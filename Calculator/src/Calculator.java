96ea2e1f353c1d520c74fbcd0bcf836da9259bac
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-10-30 19:34:13 +0100

    osztály metódus  hívás

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,9 @@
+
+public class Calculator {
+	
+    public int osszead(int a, int b){
+        return a+b;
+    }
+	
+	
+}
---
+++ Calculator/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("1+2= "+(1+2));
+
+		Calculator calc = new Calculator();
+		System.out.println("1+2= "+ calc.osszead(1 ,2));
+
+    }
+
+
+}
