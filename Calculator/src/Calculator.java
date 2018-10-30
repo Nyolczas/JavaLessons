61d35cf0006340564fa2d2f7aaad62818f5c9f98
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-10-30 19:52:25 +0100

    calc

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,9 +1,16 @@
 
 public class Calculator {
-	
-    public int osszead(int a, int b){
-        return a+b;
+
+    public int osszead(int a, int b) {
+        return a + b;
     }
-	
-	
+
+    public int osszead(int[] szamok) {
+        int temp = 0;
+        for (int i : szamok) {
+            temp += i;
+        }
+        return temp;
+    }
+
 }
--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
 		Calculator calc = new Calculator();
 		System.out.println("1+2= "+ calc.osszead(1 ,2));
+		System.out.println("1+2+3 = "+calc.osszead(new int[]{1,2,3}));
 
     }
 
