417af55729b79f50288264f0c411f81a48871424
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-10-31 16:18:48 +0100

    Evolucio

---
+++ Evolucio/src/Fajok/Faj.java
@@ -0,0 +1,13 @@
+package Fajok;
+
+public class Faj {
+    public String nev;
+
+    public Faj(){
+        nev = "valamilyen állat";
+    }
+
+    public Faj(String milyenAllat){
+        nev = milyenAllat;
+    }
+}
---
+++ Evolucio/src/Main.java
@@ -0,0 +1,12 @@
+import Fajok.Faj;
+
+public class Main {
+    public static void main(String[] args) {
+        Faj f1 = new Faj("Tigris");
+        Faj f2 = new Faj("Elefánt");
+
+
+        System.out.println(f1.nev);
+        System.out.println(f2.nev);
+    }
+}
